01c530ee63a8e2ae675f9880a614f56f297f0e9c
Kiyomi Li <kshiraiw@gmail.com>
2014-11-26 14:37:34 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready( function() {
+	$('.flex-images').flexImages({rowHeight: 140});
+$('.fancybox').fancybox({
+    'height':'500',
+    'autoDimensions':false,
+    'autoSize':false
+});
+})
